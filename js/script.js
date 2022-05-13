d7621085955f20bc1e7800d167c51e25c6e8c583
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-13 16:42:38 +0200

    setup

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*  L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+    con difficoltà 1 => tra 1 e 100
+    con difficoltà 2 => tra 1 e 81
+    con difficoltà 3 => tra 1 e 49
+    Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
+
+
+
+// chiedo all'utente a quale difficoltà vuole giocare
+
+// in base alla difficoltà scelta, genero una griglia quadrata che conterrà le celle e il giocatore potrà fare la sua partita
+
+// ogni cella conterrà un numero con un range direttamente proporzionale alla difficoltà scelta
+
+// quando l'utente darà i suoi click, le celle si coloreranno di azzurro
